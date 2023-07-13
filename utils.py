4968aac5ad40d702f2c32a57bd718387e5003058
tanwenchong <129516120+tanwenchong@users.noreply.github.com>
2023-07-13 15:27:04 +0800

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,29 @@
+def get_score(file):
+    scores=dict()
+    with open(file,'r') as f:
+        mark=False
+        for line in f:
+            if mark==False:
+                mark=True
+                continue
+            else:
+                score,name=line.split(',')
+                scores[name[:-3]]=abs(float(score))
+    return scores
+
+def get_smile(file):
+    smiles=dict()
+    with open(file,'r') as f:
+        mark=False
+        for line in f:
+            smile,name=line.split(' ')
+            smiles[name[:-3]]=smile
+    return smiles
+
+def get_dataframe(smiles,scores,ratio):
+	df_m=pd.DataFrame.from_dict(smiles, orient='index',columns=['smiles'])
+	df_s=pd.DataFrame.from_dict(scores, orient='index',columns=['scores'])
+	df=pd.concat([df_m,df_s],axis=1,join='inner')
+	df=df.sort_values(by='scores')
+	df['labels']=df['scores'].gt(df.iloc[int(len(df)*ratio)]['scores'])	
+	return df
