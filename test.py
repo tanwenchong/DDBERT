5a46d2a7b7eda2d34baa0445dac8373b3a4f3965
tanwenchong <129516120+tanwenchong@users.noreply.github.com>
2023-07-13 15:49:06 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,5 @@
 import matplotlib.pyplot as plt
-from sklearn.metrics import roc_auc_score
-from sklearn.metrics import metrics.accuracy_score
+import sklearn.metrics 
 import numpy as np
 import pandas as pd
 import torch
@@ -12,16 +11,50 @@ from transformers import AutoTokenizer, RobertaForSequenceClassification
 parser = argparse.ArgumentParser()
 parser.add_argument('-t','--test_file')
 parser.add_argument('-l','--test_label')
+parser.add_argument('-m','--model')
+
 args = parser.parse_args()
 test_file=args.test_file
 test_label=args.test_label
+model_dict=args.model
 
+tokenizer = AutoTokenizer.from_pretrained("DeepChem/ChemBERTa-10M-MLM")
+model = RobertaForSequenceClassification.from_pretrained("DeepChem/ChemBERTa-10M-MLM",num_labels=2)
+model.load_state_dict(torch.load(model_dict))
 
-test_smiles=test['smiles']
+test_smiles=get_smile(test_file)
+test_scores=get_score(test_label)
+test_data=get_dataframe(test_smiles,test_scores,ratio)
+
+test_smiles=test_data['smiles']
+
 predictions = []
 for smiles in test_smiles:
     inputs = tokenizer(smiles, return_tensors="pt", padding='max_length', truncation=True, max_length=150).to(device) 
     with torch.no_grad():
-        outputs = model(**inputs).logits
-    predict = outputs.squeeze().item()
+        outputs = model(**inputs)
+    predict = outputs.logits.argmax(axis=1)
     predictions.append(predict)
+    
+report = metrics.classification_report(test_data['labels'],predictions)
+fpr, tpr, thresholds = metrics.roc_curve(test_data['labels'],predictions)
+auc = metrics.auc(fpr,tpr)
+plt.plot(fpr,tpr,'*-')
+plt.ylabel('TPR')
+plt.xlabel('FPR')
+plt.title('ROC curve')
+print(report)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
