cfe3da18a447146ca1d98cdcd08cc0c6c1fcd784
tanwenchong <129516120+tanwenchong@users.noreply.github.com>
2023-07-13 15:26:29 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,21 @@
 import matplotlib.pyplot as plt
 from sklearn.metrics import roc_auc_score
 from sklearn.metrics import metrics.accuracy_score
+import numpy as np
+import pandas as pd
+import torch
+import argparse
+import torch.utils.data as Data
+from torch.utils.data import Dataset
+from transformers import AutoTokenizer, RobertaForSequenceClassification
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-t','--test_file')
+parser.add_argument('-l','--test_label')
+args = parser.parse_args()
+test_file=args.test_file
+test_label=args.test_label
+
 
 test_smiles=test['smiles']
 predictions = []
